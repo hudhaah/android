9e4fda074a8914688849d549751e5d8065bdfb0c
hudhaah <147602136+hudhaah@users.noreply.github.com>
2024-08-24 14:56:46 +0530

    Completed

---
+++ Login/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String VALID_USERNAME="user";
+    private static final String VALID_PASSWORD="pass";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        EditText usernameEditText=findViewById(R.id.username);
+        EditText passwordEditText=findViewById(R.id.password);
+        Button loginButton=findViewById(R.id.button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if (VALID_USERNAME.equals(username) && VALID_PASSWORD.equals(password)) {
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
